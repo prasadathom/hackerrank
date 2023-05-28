ecb78a96ac1fe97b68e0dc1550dc63db18d9a278
Prasada Thombare <60562620+prasadathom@users.noreply.github.com>
2023-05-29 02:07:04 +0530

    Create find_string.py

---
+++ DSA Easy/find_string.py
@@ -0,0 +1,8 @@
+#My solution for find a string
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)-len(sub_string)+1):
+        if string[i:i+len(sub_string)] == sub_string:
+            count = count +1    
+    return count
