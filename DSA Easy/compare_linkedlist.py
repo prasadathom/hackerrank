33af6a1580b4b0aafd8a9ff5036ad152a1a39d15
Prasada Thombare <60562620+prasadathom@users.noreply.github.com>
2023-07-12 18:43:04 +0530

    Update compare_linkedlist.py

--- DSA Easy/compare_linkedlist.py
+++ DSA Easy/compare_linkedlist.py
@@ -19,3 +19,14 @@ def compare_lists(llist1, llist2):
 
     if temp1.data==temp2.data:
         return 1
+
+
+def compare_lists(headA, headB):
+  if headA is None:
+    return 1 if headB is None else 0
+  if headB is None:
+    return 0
+  if headA.data != headB.data:
+    return 0
+  else:
+    return compare_lists(headA.next, headB.next)
