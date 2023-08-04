90c72c0741c2687d7e55735f8270d7595e2453f3
Prasada Thombare <60562620+prasadathom@users.noreply.github.com>
2023-08-04 15:25:11 +0530

    Create greedy_grid.py

---
+++ DSA Easy/greedy_grid.py
@@ -0,0 +1,15 @@
+def gridChallenge(grid): 
+    
+    grid = [list(row) for row in grid] 
+    
+    r = len(grid)
+    c= len(grid[0])
+    for i in range(r):
+        grid[i].sort()
+        
+    for j in range(c): 
+        for i in range(1,r): 
+            if not grid[i-1][j] <= grid[i][j]: 
+                return "NO"
+    
+    return "YES"
